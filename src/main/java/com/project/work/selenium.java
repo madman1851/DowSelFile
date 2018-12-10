3c6fb968796507cb3b515d857a5216f1f7c089a5
OzorakM <madman1851@gmail.com>
2018-12-10 11:47:03 +0100

    Updated selenium class, added file checking and unziping function

--- src/main/java/com/project/work/selenium.java
+++ src/main/java/com/project/work/selenium.java
@@ -3,16 +3,20 @@ package com.project.work;
 import com.gargoylesoftware.htmlunit.html.DomElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Value;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
+import java.net.URL;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 @Service
 public class selenium {
@@ -76,8 +80,8 @@ public class selenium {
     }
     
     public void pickFromMenu(String link) throws Throwable{
-        String menu = getClick();
-        WebElement menu = webDriver.findElement(By.xpath(menu));
+        String _menu = getClick();
+        WebElement menu = webDriver.findElement(By.xpath(_menu));
         Actions action = new Actions(webDriver);
         action.moveToElement(menu).perform();
         webDriver.findElement(By.linkText(link)).click();
@@ -108,10 +112,74 @@ public class selenium {
     }
 
     public void wdRun()throws Throwable{
+        checkForDriver();
         ConnectToWeb();
         InsertCredentials();
         NavigateToReports();
         DownloadReports();
         CloseAll();
     }
+
+    public void checkForDriver() {
+        try {
+            dirSearch();
+            URL website = new URL("https://sourceforge.net/projects/htmlunit/files/latest/download");
+            ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+            FileOutputStream fos = new FileOutputStream("htmlDriver.zip");
+            fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+            unzipFile("htmlDriver.zip",webDriverPath);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void unzipFile(String zipFilePath, String destDir) {
+        File dir = new File(destDir);
+        // create output directory if it doesn't exist
+        if(!dir.exists()) dir.mkdirs();
+        FileInputStream fis;
+        //buffer for read and write data to file
+        byte[] buffer = new byte[1024];
+        try {
+            fis = new FileInputStream(zipFilePath);
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while(ze != null){
+                String fileName = ze.getName();
+                File newFile = new File(destDir + File.separator + fileName);
+                System.out.println("Unzipping to "+newFile.getAbsolutePath());
+                //create directories for sub directories in zip
+                new File(newFile.getParent()).mkdirs();
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                fos.close();
+                //close this ZipEntry
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            //close last ZipEntry
+            zis.closeEntry();
+            zis.close();
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public boolean dirSearch(){
+        File f = new File(webDriverPath);
+        File[] matchingFiles = f.listFiles(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.startsWith(webDriverName);
+            }
+        });
+        if(matchingFiles!=null) return true;
+        return false;
+    }
+
 }
